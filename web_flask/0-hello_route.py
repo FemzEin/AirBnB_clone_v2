03197b913b73c0843c3039070de7251ae7ed1974
Albert James <femzein@gmail.com>
2023-04-23 04:34:11 -0700

    hello_route update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,13 +6,13 @@ Start Flask app with root route
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello():
-    return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return 'HBNB'
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    Returns a string saying hello
+    """
+    return "Hello HBNB!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
