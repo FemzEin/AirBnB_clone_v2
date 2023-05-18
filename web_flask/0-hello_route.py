d6c88dddac0d868a7ffaf3f8340a24d0bab6c161
Albert James <femzein@gmail.com>
2023-05-18 10:28:36 -0700

    overwrite

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 
 
 @app.route("/airbnb-onepage/", strict_slashes=False)
-def hello():
+def hello_hbnb():
     """
     Returns a string saying hello
     """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5000)
